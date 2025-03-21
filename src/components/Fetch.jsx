20d39db58a0cf8cd78d07bd4ed067e9f65f005a8
Akanimoh <143044085+Akanimoh12@users.noreply.github.com>
2025-03-22 06:49:55 +1200

    Update Fetch.jsx

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ColorGuessingGame.css';
 
-const colors = ['cyan', 'magenta', 'yellow', 'blue', 'turquoise', 'pink'];
+const colors = ['cyan', 'magenta', 'green', 'blue', 'turquoise', 'pink'];
 
 const shuffleArray = (array) => {
   return array.sort(() => Math.random() - 0.5);
